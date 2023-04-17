f53b483330567b44841e5cb80ab127f56af879bf
Tony Myers <tony@myers.co.nz>
2023-04-17 16:11:32 +1200

    Fixing navbar

--- src/layouts/header/SecondaryMenu.vue
+++ src/layouts/header/SecondaryMenu.vue
@@ -16,7 +16,7 @@
           <template v-for="(menu, i) in mainMenuConfig" :key="i">
             <TabPanel v-if="menu.items">
               <nav
-                class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row"
+                class="flex-row hidden pb-4 md:pb-0 md:flex md:justify-end"
               >
                 <template v-for="item in menu.items" :key="item.to">
                   <router-link
@@ -117,7 +117,7 @@ export default defineComponent({
 
     const store = useAuthStore();
     const activeClass = ref("text-gray-900 bg-gray-200");
-    const defaultClass = ref("px-4 py-2 mt-2 text-sm rounded-md dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline");
+    const defaultClass = ref("px-4 py-2 text-sm rounded-md dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:mr-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline");
 
     return {
       activeClass,
