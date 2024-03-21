73a262ab9262ceb72cfb9d15730adf5ef830ca5a
Tony Myers <tony@myers.co.nz>
2024-03-21 15:03:15 +1300

    DT Icon adjustments

--- src/components/resources/datatable/AdvDataTable.vue
+++ src/components/resources/datatable/AdvDataTable.vue
@@ -31,7 +31,7 @@
         >
           <span v-if="!simpleCreate">{{ translate("va.actions.create")}} <span class="lowercase">{{ getSingularizedLabel(resource.label) }}</span></span>
           <span v-else>
-            <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <svg class="h-4 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
             </svg>
           </span>
@@ -50,7 +50,7 @@
         >
           <span v-if="!simpleCreate">{{ translate("va.actions.create")}} {{ getSingularizedLabel(resource.label) }}</span>
           <span v-else>
-            <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <svg class="h-4 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
             </svg>
           </span>
@@ -94,7 +94,7 @@
     >
       <template v-if="showToolbar" #header>
         <div
-          class="flex flex-column md:flex-row md:justiify-content-between p-2 gap-4 dark:bg-transparent"
+          class="flex flex-column md:flex-row md:justiify-content-between p-2 gap-2 dark:bg-transparent"
         >
           <div v-if="toolbar?.search != false" class="flex w-full justify-end">
             <span class="w-full relative">
@@ -134,7 +134,7 @@
           >
             <span v-if="!simpleCreate">{{ translate("va.actions.create")}} {{ getSingularizedLabel(resource.label) }}</span>
             <span v-else>
-              <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <svg class="h-4 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
               </svg>
             </span>
@@ -151,7 +151,7 @@
           >
             <span v-if="!simpleCreate">{{ translate("va.actions.create")}} {{ getSingularizedLabel(resource.label) }}</span>
             <span v-else>
-              <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <svg class="h-4 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
               </svg>
             </span>
