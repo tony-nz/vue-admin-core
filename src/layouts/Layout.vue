1eda8d19396c12bc0523bce14b7d9293a3a872d8
Tony Myers <tony@myers.co.nz>
2024-04-07 13:33:46 +1200

    Changing layout background

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -4,7 +4,7 @@
     id="vueadmin-app"
     class="flex flex-col w-full max-h-full overflow-hidden"
     :class="{
-      'bg-gray-100': !darkMode,
+      'bg-gray-200': !darkMode,
       'bg-slate-800 dark': darkMode,
     }"
   >
@@ -20,7 +20,10 @@
     </Toolbar>
     <!-- pb-12 for footer -->
     <!-- <main class="relative flex flex-col flex-1 pb-12 overflow-auto h-full z-0" :class="displayToolbar ? '-mt-16' : '-mt-[122px]'"> -->
-    <main class="relative flex flex-col flex-1 overflow-auto h-full z-0" :class="displayToolbar ? '-mt-16' : '-mt-[120px]'">
+    <main
+      class="relative flex flex-col flex-1 overflow-auto h-full z-0"
+      :class="displayToolbar ? '-mt-16' : '-mt-[120px]'"
+    >
       <div
         id="vueadmin-content"
         :class="{
@@ -42,11 +45,7 @@
             :is="Component"
             :key="viewKey"
           /> -->
-          <component
-            v-if="route"
-            :is="Component"
-            :key="viewKey"
-          />
+          <component v-if="route" :is="Component" :key="viewKey" />
         </router-view>
       </div>
     </main>
@@ -55,9 +54,23 @@
     >
       <RouterTabs />
     </footer> -->
-    <button @click="scrollToTop" class="fixed bottom-18 right-2 p-2 rounded-md bg-emerald-500 text-white hover:bg-emerald-700 focus:bg-emerald-700 transition-colors duration-300">
-      <svg aria-hidden="true" class="w-6 h-6" focusable="false" data-prefix="fas" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z" />
+    <button
+      @click="scrollToTop"
+      class="fixed bottom-18 right-2 p-2 rounded-md bg-emerald-500 text-white hover:bg-emerald-700 focus:bg-emerald-700 transition-colors duration-300"
+    >
+      <svg
+        aria-hidden="true"
+        class="w-6 h-6"
+        focusable="false"
+        data-prefix="fas"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <path
+          fill="currentColor"
+          d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"
+        />
       </svg>
     </button>
     <!-- <button
@@ -74,7 +87,11 @@
 </template>
 
 <script lang="ts">
-import { contentWidth, displayLoader, displayToolbar } from "../core/helpers/config";
+import {
+  contentWidth,
+  displayLoader,
+  displayToolbar,
+} from "../core/helpers/config";
 import { computed, defineComponent, onBeforeMount, onMounted } from "vue";
 import { darkMode } from "../core/helpers/config";
 import { useLoading } from "vue-loading-overlay";
