a78bf23f8899b38f1e533c578ac83bd4e3c10e7c
Tony Myers <tony@myers.co.nz>
2024-05-14 09:48:09 +1200

    Removing old components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,9 +4,6 @@ import ActionBtn from "./button/ActionBtn.vue";
 // card
 import Card from "./card/Card.vue";
 
-// colorPicker
-import ColorPicker from "./colorpicker/Index.vue";
-
 // icons
 import Duotone from "./icons/Duotone.vue";
 
@@ -16,13 +13,4 @@ import Tabs from "./tabs/Tabs.vue";
 import TabPanel from "./tabs/TabPanel.vue";
 import TabPanels from "./tabs/TabPanels.vue";
 
-export {
-  ActionBtn,
-  Card,
-  ColorPicker,
-  Duotone,
-  Tab,
-  Tabs,
-  TabPanel,
-  TabPanels,
-};
+export { ActionBtn, Card, Duotone, Tab, Tabs, TabPanel, TabPanels };
