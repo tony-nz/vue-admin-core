fb2807616d70e5aa4392dd38d6e87c08d331b7c6
Tony Myers <tony@myers.co.nz>
2024-06-03 23:09:58 +1200

    Changing padding for secondary menu items

--- src/layouts/header/SecondaryMenu.vue
+++ src/layouts/header/SecondaryMenu.vue
@@ -125,7 +125,7 @@ export default defineComponent({
     const store = useAppStore();
     const activeClass = ref("text-gray-900 bg-gray-200");
     const defaultClass = ref(
-      "px-4 py-2 text-sm rounded-md dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:mr-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+      "px-2 py-2 text-sm rounded-md dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:mr-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
     );
 
     return {
