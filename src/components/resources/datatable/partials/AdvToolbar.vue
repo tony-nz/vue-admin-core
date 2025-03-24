34a694c2e9351b1ae72df54cf7803b3a5af9f364
Tony Myers <tony@myers.co.nz>
2025-03-24 14:29:04 +1300

    Moving search to toolbar

--- src/components/resources/datatable/partials/AdvToolbar.vue
+++ src/components/resources/datatable/partials/AdvToolbar.vue
@@ -32,6 +32,40 @@
       <div
         class="flex flex-column items-center w-full justify-end md:flex-row md:justiify-content-between gap-2 dark:bg-transparent"
       >
+        <div
+          v-if="showSearch || filters['global'].value"
+          class="flex w-full justify-end p-1.5"
+        >
+          <span class="flex w-full relative">
+            <i
+              class="pi pi-search absolute top-2/4 -mt-2 left-3 text-surface-400 dark:text-surface-600"
+            />
+            <InputText
+              v-model="filters['global'].value"
+              @input="debounce(onFilter, 500)"
+              id="dt_search"
+              class="pl-10 font-normal w-full min-w-[350px]"
+              placeholder="Keyword search"
+            />
+            <button
+              v-if="filters['global'].value"
+              @click="clearSearch"
+              type="button"
+              class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 dark:text-gray-400"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                class="h-5 w-5"
+              >
+                <path
+                  fill="currentColor"
+                  d="M2.93 17.07A10 10 0 1 1 17.07 2.93A10 10 0 0 1 2.93 17.07m1.41-1.41A8 8 0 1 0 15.66 4.34A8 8 0 0 0 4.34 15.66m9.9-8.49L11.41 10l2.83 2.83l-1.41 1.41L10 11.41l-2.83 2.83l-1.41-1.41L8.59 10L5.76 7.17l1.41-1.41L10 8.59l2.83-2.83z"
+                />
+              </svg>
+            </button>
+          </span>
+        </div>
         <slot></slot>
         <Button
           v-if="
@@ -129,40 +163,6 @@
           severity="info"
         />
       </div>
-    </div>
-    <div
-      v-if="showSearch || filters['global'].value"
-      class="flex w-full justify-end p-1.5"
-    >
-      <span class="flex w-full relative">
-        <i
-          class="pi pi-search absolute top-2/4 -mt-2 left-3 text-surface-400 dark:text-surface-600"
-        />
-        <InputText
-          v-model="filters['global'].value"
-          @input="debounce(onFilter, 500)"
-          id="dt_search"
-          class="pl-10 font-normal w-full min-w-[350px]"
-          placeholder="Keyword search"
-        />
-        <button
-          v-if="filters['global'].value"
-          @click="clearSearch"
-          type="button"
-          class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 dark:text-gray-400"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            class="h-5 w-5"
-          >
-            <path
-              fill="currentColor"
-              d="M2.93 17.07A10 10 0 1 1 17.07 2.93A10 10 0 0 1 2.93 17.07m1.41-1.41A8 8 0 1 0 15.66 4.34A8 8 0 0 0 4.34 15.66m9.9-8.49L11.41 10l2.83 2.83l-1.41 1.41L10 11.41l-2.83 2.83l-1.41-1.41L8.59 10L5.76 7.17l1.41-1.41L10 8.59l2.83-2.83z"
-            />
-          </svg>
-        </button>
-      </span>
     </div>
   </div>
   <slot name="header_right" />
